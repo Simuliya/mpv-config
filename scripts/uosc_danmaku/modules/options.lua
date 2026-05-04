752653d0b5ff361f31adccb2bd4bce823381174b
Simuliya <1099409644@qq.com>
2026-05-04 23:39:03 +0800

    提前更新弹幕库

--- scripts/uosc_danmaku/modules/options.lua
+++ scripts/uosc_danmaku/modules/options.lua
@@ -3,6 +3,8 @@ local opt = require("mp.options")
 -- 选项
 options = {
     -- 指定弹幕服务器地址，自定义服务需兼容 dandanplay 的 api
+    -- 可指定多个用逗号分隔的有序 api_server 列表
+    -- 支持每项使用 '|' 或 '#' 分隔备注，例如: "https://a.example.com|备用A" 或 "https://b.example.com#备用B"
     api_server = "https://api.dandanplay.net",
     -- 指定 b 站和爱腾优的弹幕获取的兜底服务器地址，主要用于获取非动画弹幕
     -- 可用： https://api.danmu.icu，https://dmku.hls.one
@@ -25,6 +27,8 @@ options = {
     fps = "60/1.001",
     -- 指定合并重复弹幕的时间间隔的容差值，单位为秒。默认值: -1，表示禁用
     merge_tolerance = -1,
+    -- 合并重复弹幕时是否强制合并类型和颜色不同的弹幕。默认值: false，表示仅合并类型和颜色相同的弹幕
+    merge_without_style = false,
     -- 指定弹幕关联历史记录文件的路径，支持绝对路径和相对路径
     history_path = "~~/danmaku-history.json",
     open_search_danmaku_menu_key = "Ctrl+d",
